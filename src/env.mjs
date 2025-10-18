4b5ee3ffeec2bcc8859dcd7442530bd6f3478705
utsav soni <utsavsoni619@gmail.com>
2025-10-18 12:29:50 +0530

    small chnages

--- src/env.mjs
+++ src/env.mjs
@@ -3,11 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    SITE_URL: z.url(),
+    // SITE_URL: z.url(),
     RESEND_API_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    SITE_URL: process.env.SITE_URL,
+    // SITE_URL: process.env.SITE_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
   },
 });
