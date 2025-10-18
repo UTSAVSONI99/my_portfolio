bbd812cb7c97ce7e3c6092b1dd2b64f7d2055c7d
utsav soni <utsavsoni619@gmail.com>
2025-10-18 12:50:58 +0530

    made changes in env

--- src/env.mjs
+++ src/env.mjs
@@ -1,13 +1,27 @@
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { z } from "zod";
+
+// export const env = createEnv({
+//   server: {
+//     // SITE_URL: z.url(),
+//     RESEND_API_KEY: z.string().min(1),
+//   },
+//   runtimeEnv: {
+//     // SITE_URL: process.env.SITE_URL,
+//     RESEND_API_KEY: process.env.RESEND_API_KEY,
+//   },
+// });
+
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // SITE_URL: z.url(),
-    RESEND_API_KEY: z.string().min(1),
+    RESEND_API_KEY: z.string().min(1, "Missing RESEND_API_KEY"),
   },
   runtimeEnv: {
-    // SITE_URL: process.env.SITE_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
   },
+  skipValidation:
+    process.env.NODE_ENV === "production" && !process.env.RESEND_API_KEY,
 });
